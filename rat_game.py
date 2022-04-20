e1e46032ac2f63d62b8eac06b24508df1ef60b14
TylerB2000 <tylerbumpd@gmail.com>
2022-04-19 21:35:25 -0500

    Various balance changes
    Fixed an issue with exp gain
    Added various balance changes

--- rat_game.py
+++ rat_game.py
@@ -24,21 +24,17 @@ class Monster:
       self.health = 0
     #Give player a level for defeating Remy so they only will face one
     if self.name == 'Remy':
-      exp = 200
       player.exp += 200
     #Increased level progression for Skelly's so that first levels feel more rewarding  
     if self.name == 'Skelly':
-      exp = round(int(self.level * 20))
-      player.exp += round(int(self.level * 20))
+      player.exp += int(self.level * 20)
     #Decreased level progression for Ogre's because they are a higher default level
     if self.clan == 'Ogre':
-      exp = round(int(self.level * 5))
-      player.exp += round(int(self.level * 5))
+      player.exp += int(self.level * 5)
     #Default exp gain
     else:
-      exp = round(int(self.level * 10))
-      player.exp += round(int(self.level * 10))
-    print('# {name} has been defeated! Your hero has gained {exp} XP! #'.format(name = self.name, exp = exp))
+      player.exp += int(self.level * 10)
+    print('# {name} has been defeated! Your hero has gained some XP! #'.format(name = self.name))
     player.lvl_up()
     
   #What should happen when a monster takes damage
@@ -73,11 +69,11 @@ class Hero:
   #Hero representation line
   def __repr__(self):
     if wep_choice == wep1:
-      return 'Your hero {name} is level {lvl}, currently does [{dmg} damage], and has |{health}/{maxhp}HP|.'.format(name = self.name, lvl = self.level, dmg = self.attack * int(round(self.level * .9)), health = self.health, maxhp = self.max_health)
+      return 'Your hero {name} is level {lvl}, currently does [{dmg} damage], and has |{health}/{maxhp}HP|.'.format(name = self.name, lvl = self.level, dmg = self.attack * int(round(self.level * .8, 0)), health = self.health, maxhp = self.max_health)
     if wep_choice == wep2:
-      return 'Your hero {name} is level {lvl}, currently does [{dmg} damage], and has |{health}/{maxhp}HP|.'.format(name = self.name, lvl = self.level, dmg = self.attack * int(round(self.level * .8)), health = self.health, maxhp = self.max_health)
+      return 'Your hero {name} is level {lvl}, currently does [{dmg} damage], and has |{health}/{maxhp}HP|.'.format(name = self.name, lvl = self.level, dmg = self.attack * int(round(self.level * .7, 0)), health = self.health, maxhp = self.max_health)
     if wep_choice == wep3:
-      return 'Your hero {name} is level {lvl}, currently does [{dmg} damage], and has |{health}/{maxhp}HP|.'.format(name = self.name, lvl = self.level, dmg = self.attack* int(round(self.level * .7)), health = self.health, maxhp = self.max_health)
+      return 'Your hero {name} is level {lvl}, currently does [{dmg} damage], and has |{health}/{maxhp}HP|.'.format(name = self.name, lvl = self.level, dmg = self.attack* int(round(self.level * .6, 0)), health = self.health, maxhp = self.max_health)
 
   #What should happen when the hero dies
   def dead(self):
@@ -94,11 +90,11 @@ class Hero:
       #Carry over exp incase hero has more that 100 exp
       self.exp = self.exp - 200
       #Increase the hero's max health and restore their HP
-      self.max_health = int(round(self.level * 4))
+      self.max_health = int(self.level * 5)
       #Set limit on max HP
-      if self.max_health > 100:
-        self.max_health = 100
-      self.health += int(round(self.max_health / 2))
+      if self.max_health > 75:
+        self.max_health = 75
+      self.health += int(round(self.max_health * .3 , 0))
       #Makes sure hero doesn't gain more than max HP
       if self.health > self.max_health:
         self.health = self.max_health
@@ -106,7 +102,7 @@ class Hero:
       #Set limit on max potions
       if self.potions > 15:
         self.potions = 15
-      print('*Congratulations, your hero is now level {level}!* \n{name}\'s damage and max health has been increased! {name} has also restored half of their health and gained 2 potions!'.format(level = self.level, name = self.name))
+      print('*Congratulations, your hero is now level {level}!* \n{name}\'s damage and max health has been increased! \n{name} has gained 2 potions and a third of their health has been restored!'.format(level = self.level, name = self.name))
     else:
       print('{name} now needs {exp} more XP to level up'.format(name = self.name, exp = int(200 - self.exp)))
 
@@ -144,13 +140,13 @@ class Hero:
     dam = 0
     #Determines crit chance based on weapon
     if self.weapon == wep1:
-      dam = self.attack * int(round(self.level * .9))
+      dam = self.attack * int(round(self.level * .8, 0))
       crit = random.randint(1, 2)
     elif self.weapon == wep2:
-      dam = self.attack * int(round(self.level * .8))
+      dam = self.attack * int(round(self.level * .7, 0))
       crit = random.randint(1, 3)
     elif self.weapon == wep3:
-      dam = self.attack * int(round(self.level * .7))
+      dam = self.attack * int(round(self.level * .6, 0))
       crit =  random.randint(1, 10)
     if crit == 1:
       print('{Critical hit!}')
@@ -164,7 +160,7 @@ class Hero:
   def use_potion(self):
     if self.potions > 0:
       self.potions -= 1
-      self.health += 10
+      self.health += int(round(self.max_health * .35))
       print('\n{name} used a health potion'.format(name = self.name))
       #What should happen if healed health exceeds max health
       if self.health >= self.max_health:
@@ -203,9 +199,9 @@ reset = True
 #Encounter commands
 while reset == True:
   #Get user input for encounter action
-  fight = input('\n{mon} \n{hero} \nWhat will you do? Type \'Attack\', \'Block\'(75% chance to reflect half damage), or \'Heal\'(Heals |10HP|: You have {pot} potions). \n> '.format(mon = mon, hero = hero, pot = hero.potions)).title()    
+  fight = input('\n{mon} \n{hero} \nWhat will you do? Type \'Attack\', \'Block\'(75% chance to reflect half damage), or \'Heal\'(Heals |{hp}HP|: You have {pot} potions). \n> '.format(mon = mon, hero = hero, hp = int(round(hero.max_health * .3, 0)), pot = hero.potions)).title()    
   while fight != 'Attack' and fight != 'Block' and fight != 'Heal':
-    fight  = input('Invalid command, please try again. \n{mon} \n{hero} \nWhat will you do? Type \'Attack\'(deal [{dmg} damage]), \'Block\'(75% chance to reflect half damage), or \'Heal\'(Heals |10HP|: You have {pot} potions). \n> '.format(mon = mon, hero = hero, pot = hero.potions, dmg = hero.attack * int(round(hero.level)))).title()
+    fight  = input('Invalid command, please try again. \n{mon} \n{hero} \nWhat will you do? Type \'Attack\'(deal [{dmg} damage]), \'Block\'(75% chance to reflect half damage), or \'Heal\'(Heals |{hp}HP|: You have {pot} potions). \n> '.format(mon = mon, hero = hero, hp = int(round(hero.max_health * .3, 0)), pot = hero.potions, dmg = hero.attack * int(hero.level))).title()
   #Determine what should happen if the attack command is chosen
   if fight == 'Attack':
     reset = False
@@ -224,20 +220,20 @@ while reset == True:
     #Spawns a new enemy if the current one is dead
     while mon.is_dead == True:
       #Determines what enemy to spawn based on hero level
-      if 1 < hero.level <= 5:
+      if 1 < hero.level <= 3:
         m1Lvl = random.randint(2, 5)
         mon = Monster('Skelly', 'Skeleton', m1Lvl)
-      if 5 < hero.level <= 10:
+      if 3 < hero.level <= 10:
         m2Lvl = random.randint(6, 10)
         mon = Monster('Skully', 'Skeleton', m2Lvl)
       if 10 < hero.level <= 15:
-        m3Lvl = random.randint(11, 20)
+        m3Lvl = random.randint(15, 20)
         mon = Monster('Grog', 'Ogre', m3Lvl)
       if 15 < hero.level <= 20:
         m4Lvl = random.randint(20, 30)
         mon = Monster('Greg', 'Ogre', m4Lvl)
       if hero.level > 20:
-        mon = Monster('Big Cheese', 'Rat Mob', 40)
+        mon = Monster('Big Cheese', 'Rat Mob', 50)
       #Resets monster death buffer so a new one can spawn  
       mon.is_dead == False
     #Lose condition sequence break
